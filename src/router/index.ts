b3641aa4ef2055a15e4728ed8b79d6d5b8980a70
liangyang <771234218@qq.com>
2022-10-04 19:23:12 +0800

    base

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,45 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from "../views/Home.vue"
-import Layout from '../layout/index.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Layout from "../layout/index.vue";
 const routes = [
-    {
-        path: "/", name: "Home",
-        component: Layout,
-        children: [{
-            path: "home", name: "Home",
-            component: Home,
-        }
-
-        ]
-    },
-    {
-        path: "/about", name: "About",
-        component: Layout,
-        children: [{
-            path: "index", name: "About",
-            component: () => import('../views/About.vue') 
-        }
-
-        ]
-    }, 
-    { path: "/login", name: "Login", meta: { title: '登录' }, component: () => import('../views/Login/index.vue') },
+  {
+    path: "/",
+    name: "Home",
+    component: Layout,
+    children: [
+      {
+        path: "home",
+        name: "Home",
+        component: Home,
+      },
+    ],
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: Layout,
+    children: [
+      {
+        path: "info",
+        name: "About",
+        component: () => import("../views/About.vue"),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    name: "Login",
+    meta: { title: "登录" },
+    component: () => import("../views/Login/index.vue"),
+  },
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
+  history: createWebHashHistory("/admin/"),
+  routes,
 });
 // router.beforeEach((to, from, next) => {
 //     // to and from are both route objects. must call `next`.
 //     next('/login')
 // })
-export default router
-
+export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/'
+  base:'/admin/',
+  resolve: {
+    alias: {
+      '@': path.resolve('./src') // @代替src
+    }
+  }
 })
