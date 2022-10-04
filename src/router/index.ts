d160b69ec5158a4586c4ef6271fa01173f1ec8dd
liangyang <771234218@qq.com>
2022-10-04 18:59:09 +0800

    edit

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,7 +26,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHashHistory("/admin/"),
     routes
 });
 // router.beforeEach((to, from, next) => {
