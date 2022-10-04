1302b874b45e677ef4469a237dab453df043ef63
liangyang <771234218@qq.com>
2022-10-04 20:07:41 +0800

    edit

--- src/router/index.ts
+++ src/router/index.ts
@@ -35,7 +35,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory("/admin/"),
+  history: createWebHashHistory(), //"/admin/"
   routes,
 });
 // router.beforeEach((to, from, next) => {
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/admin/',
+  //base:'/admin/',
   resolve: {
     alias: {
       '@': path.resolve('./src') // @代替src
